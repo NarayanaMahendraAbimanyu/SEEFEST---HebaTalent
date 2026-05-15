55830ad01d692ac7b5e8d4a752a51d69ff1f6d16
Narayana <narayanamahendraabimanyu@gmail.com>
2026-05-15 08:26:32 +0700

    installing tailwindcss

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import "../styles/index.css";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <img src={logo} alt="logo" className="h-12 w-auto"/>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
